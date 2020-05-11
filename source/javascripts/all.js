ed6471e5b27fd579a7bd6c4a3077626ee709bae7
LocNguyen <phoenician93@gmail.com>
2020-05-11 11:18:38 +0700

    remove all ignored words

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -101,7 +101,7 @@ t.stemmer=function(){var t={ational:"ate",tional:"tion",enci:"ence",anci:"ance",
  * lunr.stopWordFilter
  * Copyright (C) 2014 Oliver Nightingale
  */
-t.stopWordFilter=function(e){return-1===t.stopWordFilter.stopWords.indexOf(e)?e:void 0},t.stopWordFilter.stopWords=new t.SortedSet,t.stopWordFilter.stopWords.length=119,t.stopWordFilter.stopWords.elements=["","a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"],t.Pipeline.registerFunction(t.stopWordFilter,"stopWordFilter"),/*!
+t.stopWordFilter=function(e){return-1===t.stopWordFilter.stopWords.indexOf(e)?e:void 0},t.stopWordFilter.stopWords=new t.SortedSet,t.stopWordFilter.stopWords.length=119,t.stopWordFilter.stopWords.elements=["",],t.Pipeline.registerFunction(t.stopWordFilter,"stopWordFilter"),/*!
  * lunr.trimmer
  * Copyright (C) 2014 Oliver Nightingale
  */
